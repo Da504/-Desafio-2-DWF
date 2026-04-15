4236029f30e6f6f608d2cd01b77c96309f61e84e
Da504 <dam342296@gmail.com>
2026-04-14 23:39:35 -0600

    Agregando a la clase profesor

--- src/main/java/sv/edu/udb/model/Profesor.java
+++ src/main/java/sv/edu/udb/model/Profesor.java
@@ -1,4 +1,21 @@
 package sv.edu.udb.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profesor")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
 }
